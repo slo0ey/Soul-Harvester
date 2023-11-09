e42d18a6ec69b9b1fcde42d4c1f0e0a00c37d985
slo0ey <rurchi1206@gmail.com>
2023-11-10 00:00:06 +0900

    부드러운 회전 구현중..

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,25 +2,46 @@ extends CharacterBody2D
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 
-const H_SPEED := 150
+const SPEED := 150
+const ROTATE_SPEED := 10
+
+var prev_angle := 0.0
 
 func _physics_process(delta):
-	var h_dir := Input.get_axis("move_left", "move_right")
+	var direction := move_direction()
 	
-	if h_dir == 0:
-		apply_friction()
-	else:
-		if h_dir < 0:
-			animated_sprite_2d.flip_h = true
-		else:
-			animated_sprite_2d.flip_h = false
-			
-		apply_acceleration(h_dir)
+	direction = apply_rotation(direction, delta)
+	
+	velocity.x = apply_acceleration(velocity.x, direction.x, delta)
+	velocity.y = apply_acceleration(velocity.y, direction.y, delta)
+	
+	if direction.x < 0:
+		animated_sprite_2d.flip_h = true
+	elif direction.x > 0:
+		animated_sprite_2d.flip_h = false
 	
 	move_and_slide()
 
-func apply_friction():
-	velocity.x = move_toward(velocity.x, 0, 10)
+func move_direction() -> Vector2:
+	var direction := Vector2(
+		Input.get_axis("move_left", "move_right"),
+		Input.get_axis("move_up", "move_down")
+	)
+	
+	if direction:
+		return direction.normalized()
+	else:
+		return direction
 
-func apply_acceleration(amount):
-	velocity.x = move_toward(velocity.x, H_SPEED * amount, 10)
+func apply_rotation(direction: Vector2, delta: float) -> Vector2:
+	var angle := lerpf(prev_angle, direction.angle(), delta * ROTATE_SPEED)
+	print(angle)
+	
+	if direction:
+		direction = direction.from_angle(angle)
+	prev_angle = angle
+	
+	return direction
+
+func apply_acceleration(base: float, dir: float, delta: float) -> float:
+	return move_toward(base, dir * SPEED, delta * 1000)
