fba221785ca9676118dfef94860bf6cb6d258874
slo0ey <rurchi1206@gmail.com>
2023-11-10 23:53:44 +0900

    잔상 이펙트 추가

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,15 +2,10 @@ extends CharacterBody2D
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 
-const SPEED := 150
-const ROTATE_SPEED := 10
-
-var prev_angle := 0.0
+const SPEED := 250
 
 func _physics_process(delta):
 	var direction := move_direction()
-	
-	direction = apply_rotation(direction, delta)
 	
 	velocity.x = apply_acceleration(velocity.x, direction.x, delta)
 	velocity.y = apply_acceleration(velocity.y, direction.y, delta)
@@ -32,16 +27,6 @@ func move_direction() -> Vector2:
 		return direction.normalized()
 	else:
 		return direction
-
-func apply_rotation(direction: Vector2, delta: float) -> Vector2:
-	var angle := lerpf(prev_angle, direction.angle(), delta * ROTATE_SPEED)
-	print(angle)
-	
-	if direction:
-		direction = direction.from_angle(angle)
-	prev_angle = angle
-	
-	return direction
 
 func apply_acceleration(base: float, dir: float, delta: float) -> float:
 	return move_toward(base, dir * SPEED, delta * 1000)
